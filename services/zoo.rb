7ae4f5645055e8ef9d028ae7da11b1207ff71976
Ssamael <hikokoro06@gmail.com>
2018-06-12 15:51:12 +0200

    Defined animal classes

--- services/zoo.rb
+++ services/zoo.rb
@@ -2,3 +2,33 @@ class Zoo
   def inventory
   end
 end
+
+class Animal
+  def initialize (name, description)
+    @name = name
+    @description = description
+  end
+end
+
+class Alligator < Animal
+end
+
+class Otter < Animal
+end
+
+class Gorilla < Animal
+  def initialize (name, description, orgin)
+    super(name, description)
+    @orgin = orgin
+  end
+end
+
+class Penguin < Animal
+end
+
+class Lemur < Animal
+  def initialize (name, description, kind)
+    super(name, description)
+    @kind = kind
+  end
+end
