37ac3192b8f38d02ffa937289f3086642c6264a6
Ssamael <hikokoro06@gmail.com>
2018-06-12 22:55:17 +0200

    Added support for having 2 animals

--- models/animal.rb
+++ models/animal.rb
@@ -1,10 +1,12 @@
 class Animal
+  attr_reader :name
+  
   def initialize(name:, description:)
     @name = name
     @description = description
   end
   
   def representation
-    "#{self.class.name} #{@name} #{@description.downcase}"
+    "#{self.class.name} #{name} #{@description.downcase}"
   end
 end
--- services/zoo.rb
+++ services/zoo.rb
@@ -9,17 +9,26 @@ class Zoo
   def initialize
     @animals = []
   end
-  
+
   def add_animals(animals)
     @animals += animals
   end
-  
+
   def inventory
     if @animals.count == 0
       'Oh no! There are no animals in our ZOO!'
-    else
+    elsif @animals.count == 1
       'There is only one animal in our ZOO! ' +
         @animals.first.representation + '. Hope you will enjoy your visit!'
+    else
+      'For now there are only 2 animals here! ' +
+        sorted_animals.map(&:representation).join(' and ') + '. Hope you will enjoy your visit!'
     end
+  end
+
+  private
+
+  def sorted_animals
+    @animals.sort_by(&:name)
   end
 end
